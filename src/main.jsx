c84f69ea772f6d3083193e9f0efbc15abb0ae12c
rapha~El <111363603+RalphFred@users.noreply.github.com>
2023-08-29 04:03:33 +0100

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <div>WHat the hell is this?</div>
   </React.StrictMode>,
 )
