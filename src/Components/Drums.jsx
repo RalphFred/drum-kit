c244785b0ca0f4680018875bf8ab883ea98dacb8
rapha~El <111363603+RalphFred@users.noreply.github.com>
2023-08-29 03:23:54 +0100

    Yeah

--- src/Components/Drums.jsx
+++ src/Components/Drums.jsx
@@ -4,7 +4,7 @@ import Drum from './Drum';
 const Drums = () => {
 
   return (
-    <div className="w-screen flex flex-wrap justify-around items-center mt-16 px-2">
+    <div className="w-full flex flex-wrap justify-around items-center mt-16 px-2">
         {sources.map((source, index) => (
           <Drum key={index} source={source}/>
         ))
